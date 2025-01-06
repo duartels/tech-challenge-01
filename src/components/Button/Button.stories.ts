7546e1e7633d9fc3f0d95f7369bf71547dcd0c35
Luan Duarte <luansouza789@gmail.com>
2025-01-06 17:51:41 -0300

    feat: storybook added

---
+++ src/components/Button/Button.stories.ts
@@ -0,0 +1,23 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { fn } from '@storybook/test';
+
+import { Button } from './';
+
+const meta = {
+  title: 'Button',
+  component: Button,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+  args: { onClick: fn() },
+} satisfies Meta<typeof Button>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Example: Story = {
+  args: {
+    children: 'teste'
+  },
+};
