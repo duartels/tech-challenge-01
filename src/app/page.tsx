26be1f943c53932c6816655f8603a30bb408e3c3
Luan Duarte <luansouza789@gmail.com>
2025-01-06 14:13:49 -0300

    feat: add props to button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
+'use client'
 import { Button } from '@/components/Button'
 
 export default function Home() {
+  const teste = () => {
+    console.log('teste')
+  }
+
   return (
     <div className="flex items-center justify-center h-screen">
-      <Button>Concluir transação</Button>
+      <Button onClick={teste}>Concluir transação</Button>
     </div>
   )
 }
--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,11 +1,14 @@
+'use client'
+
 type ButtonProps = {
   children: React.ReactNode
-}
+} & React.HTMLAttributes<HTMLButtonElement>
 
-export const Button = ({ children }: ButtonProps) => {
+export const Button = ({ children, ...props }: ButtonProps) => {
   return (
     <button
-      className={`w-[144px] sm:w-[250px] h-12 bg-tertiary rounded-lg flex justify-center items-center px-12 font-inter text-primary font-bold hover:bg-primary hover:text-tertiary transition delay-100`}
+      className={`w-[144px] sm:w-[250px] h-12 bg-tertiary rounded-lg flex justify-center items-center px-12 text-primary font-bold hover:bg-primary hover:text-tertiary transition delay-100`}
+      {...props}
     >
       {children}
     </button>
